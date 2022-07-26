eb9ffb9da06aa785bd070a1fe1ccf34bacfed044
irvinzato <multizato@hotmail.com>
2022-07-26 11:33:41 -0500

    Agregue estilos a mi componente Testimonio

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import '../styles/Testimonio.css';
 
 //Las exportaciones nombradas llevan "export" antes del "function" y donde se importa llevaria {}
 function Testimonio() {
   return (
     <div className='testimonial-container'>
       <img className='testimonial-img' 
-           src={ require('../images/logo-rojo.jpg') } alt='logo rojo' />
+           src={ require('../images/avatar-mujer.png') } alt='logo rojo' />
       <div className='testimonialText-container'>
         <p className='testimonial-name'>Angeles López en México</p>
         <p className='testimonial-position'>Ingeniera de Software</p>
-        <p className='testimonial-text'>Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify.</p>
+        <p className='testimonial-text'>"Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
       </div>
     </div>
   );
