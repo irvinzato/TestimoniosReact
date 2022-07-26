c70719de90f3d7a1854d533db4949023b1c27c5a
irvinzato <multizato@hotmail.com>
2022-07-26 11:57:37 -0500

    Uso de props en componente y renderizacion

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ function App() {
     <div className='App'>
       <div className='principal-content'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio
+          image='mujer' 
+          name='Angeles López'
+          country='México'
+          position='Ingeniera de Software'
+          company='Spotify'
+          testimonial='Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify.' />
+        
+        <Testimonio
+          image='mujer2' 
+          name='Jade López'
+          country='Singapur'
+          position='Ingeniera de Software'
+          company='ChatDesk'
+          testimonial='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mi conocimiento de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
+        
+        <Testimonio
+          image='hombre' 
+          name='Irving Rivera'
+          country='Noruega'
+          position='Ingeniero de Software'
+          company='Amazon'
+          testimonial='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando en los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
       </div>
     </div>
   );
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import '../styles/Testimonio.css';
 
 //Las exportaciones nombradas llevan "export" antes del "function" y donde se importa llevaria {}
-function Testimonio() {
+function Testimonio( props ) {
   return (
     <div className='testimonial-container'>
       <img className='testimonial-img' 
-           src={ require('../images/avatar-mujer.png') } alt='logo rojo' />
+           src={ require(`../images/avatar-${ props.image }.png`) } alt='logo rojo' />
       <div className='testimonialText-container'>
-        <p className='testimonial-name'>Angeles López en México</p>
-        <p className='testimonial-position'>Ingeniera de Software</p>
-        <p className='testimonial-text'>"Siempre me ha costado aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+        <p className='testimonial-name'>{ props.name } en { props.country } </p>
+        <p className='testimonial-position'>{ props.position } en { props.company } </p>
+        <p className='testimonial-text'> "{ props.testimonial }" </p>
       </div>
     </div>
   );
